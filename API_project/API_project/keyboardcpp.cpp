c60ce2364cd4dc2e87340ff9d1e7578c49dae76c
maeng-gu <38423200+maeng-gu@users.noreply.github.com>
2019-05-27 14:40:36 +0900

    move_0

---
+++ API_project/API_project/keyboardcpp.cpp
@@ -0,0 +1,83 @@
+#include <windows.h>
+#include <TCHAR.H>
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg,
+	WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	static int x, y;
+
+	switch (iMsg) {
+	case WM_CREATE:
+		x = 20; y = 20;
+		return 0;
+
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		Ellipse(hdc, x - 20, y - 20, x + 20, y + 20);
+		EndPaint(hwnd, &ps);
+		return 0;
+
+	case WM_KEYDOWN:
+		if (wParam == VK_RIGHT) x += 40;
+
+		if (wParam == VK_LEFT) x -= 40;
+
+		if (wParam == VK_UP) y -= 40;
+
+		if (wParam == VK_DOWN) y += 40;
+
+		if (wParam == VK_HOME) {
+			x = 20;
+			y = 20;
+		}
+
+		InvalidateRgn(hwnd, NULL, TRUE);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		return DefWindowProc(hwnd, iMsg, wParam, lParam);
+	}
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpszCmdLine, int nCmdShow)
+{
+	HWND     hwnd;
+	MSG     msg;
+	WNDCLASS WndClass;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.lpszMenuName = NULL;
+	WndClass.lpszClassName = _T("Window Class Name");
+	RegisterClass(&WndClass);
+	hwnd = CreateWindow(_T("Window Class Name"),
+		_T("Window Title Name"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
